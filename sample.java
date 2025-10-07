79fb93dd1c78a77a8ed56463e90cb4a25e2f6258
arshdeep-16 <dparshdeep16@gmail.com>
2025-10-07 10:33:10 +0530

    Update sample.java

--- sample.java
+++ sample.java
@@ -1,7 +1,7 @@
 public class sample {
 
 
-  public static void mai(String[] args) {
+  public static void main(String[] args) {
 
     System.out.println("Hey this is for testing purpose only");
    // System.out.println(myObj2.x);
